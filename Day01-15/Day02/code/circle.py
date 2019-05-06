98752e611fae9a5861dd6c51c856f57332467a11
student-ruihao <47907026+student-ruihao@users.noreply.github.com>
2019-05-06 15:16:56 +0800

    Update circle.py

--- Day01-15/Day02/code/circle.py
+++ Day01-15/Day02/code/circle.py
@@ -11,5 +11,5 @@ import math
 radius = float(input('请输入圆的半径: '))
 perimeter = 2 * math.pi * radius
 area = math.pi * radius * radius
-print('周长: %.2f' % perimeter)
+print('周长: %.2f' % perimeter) #%.2f表示输出数据小数点的位数
 print('面积: %.2f' % area)
